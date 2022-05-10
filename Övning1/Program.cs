10cd4d1d4e6188c57dc89f3882a0680754b30ffb
kma <kma@RUTAN>
2022-05-10 16:50:02 +0200

    Add project files.

---
+++ Övning1/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> namn = new List<string>();
+List<string> lön = new List<string>();
+int antal = 0;
+string enlön;
+string ettnamn;
+String svar1 = " ";
+string svar2 = " ";  
+
+while (svar1 != "n")
+{
+    antal = antal + 1; ;
+    Console.WriteLine("skriv in namn");
+    ettnamn = Console.ReadLine();
+    namn.Add(ettnamn);
+    Console.WriteLine("skriv in lön");
+    enlön = Console.ReadLine();
+    lön.Add(enlön);
+    Console.WriteLine("Fler inmatningar ? j/n enter");
+    svar1 = Console.ReadLine();
+    
+}
+
+Console.WriteLine("namn     "+"Lön   ");
+for (int i = 0; i < antal; i++)
+    Console.WriteLine(namn[i] + "     " + lön[i]);
+    
